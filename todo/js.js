c4c2ffcce83de33a053c0de0f852a925df674381
sutharsang631 <sutharsang631@gmail.com>
2023-06-27 15:03:01 +0530

    initial commit

---
+++ todo/js.js
@@ -0,0 +1,16 @@
+$(document).on('click', '.removeTaskButton', function() {
+    var taskText = $(this).siblings('.taskText').text();
+    var currentUser = localStorage.getItem('currentUser');
+
+    if (taskText && currentUser) {
+      var user = JSON.parse(localStorage.getItem(currentUser));
+      var index = user.tasks.indexOf(taskText);
+
+      if (index !== -1) {
+        user.tasks.splice(index, 1);
+        localStorage.setItem(currentUser, JSON.stringify(user));
+        $(this).closest('li').remove();
+      }
+    }
+  })
+});
